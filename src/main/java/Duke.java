6bef0eddad7b9d4d745c62b44941a67c59044061
Johnson-Yee <e0494363@u.nus.edu>
2020-09-10 02:54:13 +0800

    Level 5 Commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,53 +1,66 @@
 import java.util.Scanner;
-//import java.util.Arrays;
+import java.util.Arrays;
 
 public class Duke {
+    /*Command Prefixes*/
+    private static final String COMMAND_LIST = "list";
 
+    /*Delimiter Prefixes*/
+    private static final String EVENT_QUALIFIER = "/at";
+    private static final String DEADLINE_QUALIFIER = "/by";
+
+    /*Constants used*/
+    private static final int MAX_TASKS = 100;
+
+    //private static boolean hasExited = false;
     private static final Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
         String userInput = "";
-        Task[] currentTasks = new Task[100];
+        Task[] currentTasks = new Task[MAX_TASKS];
         int numOfTasks = 0;
+        boolean isOngoing = true;
 
         showWelcomeMessage();
 
-        while(!userInput.equals("bye")) {
+        while(isOngoing) {
             userInput = getUserInput();
             String[] splitUserInput = splitCommands(userInput);
-
-            switch (splitUserInput[0]) {
-
-                case "bye":
-                    exitProgram();
-                    break;
-
-                case "list":
-                    commandList(currentTasks, numOfTasks);
-                    break;
-
-                case "done":
-                    markAsDone(currentTasks, numOfTasks, splitUserInput[1]);
-                    break;
-
-                case "deadline":
-                    numOfTasks = commandDeadline(currentTasks, numOfTasks, splitUserInput[1]);
-                    break;
-
-                case "todo":
-                    numOfTasks = commandToDo(currentTasks, numOfTasks, splitUserInput);
-                    break;
-
-                case "event":
+            switch (splitUserInput[0].toLowerCase()) {
+            case "bye":
+                printExitMessage();
+                isOngoing = false;
+                break;
+            case "list":
+                commandList(currentTasks, numOfTasks);
+                break;
+            case "done":
+                markAsDone(currentTasks, numOfTasks, splitUserInput[1]);
+                break;
+            case "deadline":
+                numOfTasks = commandDeadline(currentTasks, numOfTasks, splitUserInput[1]);
+                break;
+            case "todo":
+                numOfTasks = commandToDo(currentTasks, numOfTasks, splitUserInput[1]);
+                break;
+            case "event":
+                try {
                     numOfTasks = commandEvent(currentTasks, numOfTasks, splitUserInput[1]);
-                    break;
-
-                case "help":
-                    commandHelp();
-                    break;
-
-                default:
-                    invalidCommand();
+                } catch (DukeException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case "help":
+                commandHelp();
+                break;
+            default:
+                try {
+                    throw new DukeException("UNINTELLIGIBLE_COMMAND");
+                } catch (DukeException exception) {
+                    printSeparator();
+                    System.out.println(exception);
+                    printSeparator();
+                }
             }
         }
     }
@@ -59,57 +72,144 @@ public class Duke {
      *           MAIN COMMANDS
      * ===========================================
      */
+
+    /**
+     * Prints all tasks in task list
+     *
+     * @param currentTasks  Tasks Array of all tasks
+     */
     private static void commandList(Task[] currentTasks, int numOfTasks) {
         printSeparator();
+        if(numOfTasks == 0){
+            System.out.println("You have zero task at hand!");
+        }
         for(int i = 0; i < numOfTasks; i++){
             System.out.println((i+1) + "." +addBrackets(currentTasks[i].getType()) +
                     addBrackets(currentTasks[i].getStatusIcon()) + " " + currentTasks[i].getDescription());
         }
         printSeparator();
     }
+    /**
+     * Adds a Deadline task to the currentTasks Task array
+     *
+     * @param currentTasks  Tasks Array of all tasks
+     * @param numOfTasks  Number of tasks
+     * @param userInput User input
+     * @return numOfTasks Updated number of tasks
+     */
+    private static int commandDeadline(Task[] currentTasks, int numOfTasks, String userInput) {
+        try {
+            /*If user input contains only one word*/
+            if (userInput.length() < 1) {
+                throw new DukeException("MISSING_DESCRIPTION");
+            }
+            /*Missing qualifier*/
+            if (!userInput.contains("/by")) {
+                throw new DukeException("MISSING_QUALIFIER");
+            }
+            String[] splitInfoAndDeadline = userInput.trim().split(DEADLINE_QUALIFIER,2 );
+            /*If user input has no description*/
+            if (splitInfoAndDeadline[0].isEmpty()) {
+                throw new DukeException("MISSING_DESCRIPTION");
+            }
+            /*If user input has no deadline*/
+            if (splitInfoAndDeadline[1].isEmpty()) {
+                throw new DukeException("MISSING_INFO");
+            }
+            currentTasks[numOfTasks] = new Deadline(splitInfoAndDeadline[0], splitInfoAndDeadline[1]);
+            printSeparator();
+            System.out.println("Got it. I've added this task: \n" + "  " + addBrackets(currentTasks[numOfTasks].getType())
+                    + addBrackets(currentTasks[numOfTasks].getStatusIcon()) +" " + splitInfoAndDeadline[0] + " (by: " +
+                    splitInfoAndDeadline[1] +  ")\n" + "Now you have " +(numOfTasks +1)+ (numOfTasks == 0 ? " task" :
+                    " tasks" ) +" in the list");
+            printSeparator();
+            numOfTasks++;
 
-    private static int commandDeadline(Task[] currentTasks, int numOfTasks, String s) {
-        String[] splitInfoAndDeadline = s.trim().split("/by",2 );
-
-        currentTasks[numOfTasks] = new Deadline(splitInfoAndDeadline[0], splitInfoAndDeadline[1]);
-        printSeparator();
-        System.out.println("Got it. I've added this task: \n" + "  " + addBrackets(currentTasks[numOfTasks].getType())
-                + addBrackets(currentTasks[numOfTasks].getStatusIcon()) +" " + splitInfoAndDeadline[0] + " (by: " +
-                splitInfoAndDeadline[1] +  ")\n" + "Now you have " +(numOfTasks +1)+ (numOfTasks == 0 ? " task" : " tasks" )
-                +" in the list");
-        printSeparator();
-        numOfTasks++;
+        } catch (DukeException error) {
+            printSeparator();
+            System.out.println(error);
+            printSeparator();
+        }
         return numOfTasks;
     }
+    /**
+     * Adds a ToDo task to the currentTasks Task array
+     *
+     * @param currentTasks  Tasks Array of all tasks
+     * @param numOfTasks  Number of tasks
+     * @param splitUserInput User input
+     * @return numOfTasks Updated number of tasks
+     */
+    private static int commandToDo(Task[] currentTasks, int numOfTasks, String splitUserInput) {
+        try {
+            if(splitUserInput.isBlank()){
+                throw new DukeException("MISSING_DESCRIPTION");
+            }
+            System.out.println(splitUserInput + splitUserInput.length());
+            currentTasks[numOfTasks] = new ToDo(splitUserInput);
+            printSeparator();
+            System.out.println("Got it. I've added this task: \n" + "  " + addBrackets(currentTasks[numOfTasks].getType())
+                    + addBrackets(currentTasks[numOfTasks].getStatusIcon()) + " " + splitUserInput + " \n" + "Now you have "
+                    + (numOfTasks + 1) + (numOfTasks == 0 ? " task" : " tasks") + " in the list");
+            numOfTasks++;
+            printSeparator();
 
-    private static int commandToDo(Task[] currentTasks, int numOfTasks, String[] splitUserInput) {
-        currentTasks[numOfTasks] = new ToDo(splitUserInput[1]);
-        printSeparator();
-        System.out.println("Got it. I've added this task: \n" +"  " + addBrackets(currentTasks[numOfTasks].getType())
-                + addBrackets(currentTasks[numOfTasks].getStatusIcon()) + " "+ splitUserInput[1] + " \n" + "Now you have "
-                + (numOfTasks +1) + (numOfTasks == 0 ? " task" : " tasks" ) +" in the list");
-        numOfTasks++;
-        printSeparator();
+        }catch (DukeException error){
+            printSeparator();
+            System.out.println(error);
+            printSeparator();
+        }
         return numOfTasks;
     }
+    /**
+     * Adds a ToDo task to the currentTasks Task array
+     *
+     * @param currentTasks  Tasks Array of all tasks
+     * @param numOfTasks  Number of tasks
+     * @param userInput User input
+     * @return numOfTasks Updated number of tasks
+     */
+    private static int commandEvent(Task[] currentTasks, int numOfTasks, String userInput) throws DukeException{
+        try {
+            /*If user input contains only one word*/
+            if(userInput.length() < 1){
+                throw new DukeException("MISSING_DESCRIPTION");
+            }
+            /*Missing qualifier*/
+            if(!userInput.contains("/at")){
+                throw new DukeException("MISSING_QUALIFIER");
+            }
+            String[] splitInfoAndDate = userInput.trim().split(EVENT_QUALIFIER, 2);
+            /*If user input has no description*/
+            if(splitInfoAndDate[0].isEmpty()){
+                throw new DukeException("MISSING_DESCRIPTION");
+            }
+            /*If user input has no event date*/
+            if(splitInfoAndDate[1].isEmpty()){
+                throw new DukeException("MISSING_INFO");
+            }
 
-    private static int commandEvent(Task[] currentTasks, int numOfTasks, String s) {
-        String[] splitInfoAndDate = s.trim().split("/at",2 );
-        currentTasks[numOfTasks] = new Event(splitInfoAndDate[0], splitInfoAndDate[1] );
-        printSeparator();
-        System.out.println("Got it. I've added this task: \n" + "  " + addBrackets(currentTasks[numOfTasks].getType())
-                + addBrackets(currentTasks[numOfTasks].getStatusIcon()) +" " + splitInfoAndDate[0] + " (at: " +
-                splitInfoAndDate[1] +  ")\n" + "Now you have " + (numOfTasks +1) + (numOfTasks == 0 ? " task" : " tasks" )
-                +" in the list");
-        numOfTasks++;
-        printSeparator();
+            System.out.println(Arrays.toString(splitInfoAndDate));
+            currentTasks[numOfTasks] = new Event(splitInfoAndDate[0], splitInfoAndDate[1]);
+            printSeparator();
+            System.out.println("Got it. I've added this task: \n" + "  " + addBrackets(currentTasks[numOfTasks].getType())
+                    + addBrackets(currentTasks[numOfTasks].getStatusIcon()) + " " + splitInfoAndDate[0] + " (at: " +
+                    splitInfoAndDate[1] + ")\n" + "Now you have " + (numOfTasks + 1) + (numOfTasks == 0 ? " task" : " tasks")
+                    + " in the list");
+            numOfTasks++;
+            printSeparator();
+        } catch (DukeException error) {
+            printSeparator();
+            //error.printStackTrace();
+            System.out.println(error);
+            printSeparator();
+        }
         return numOfTasks;
     }
-
-    //List of valid commands
+    /*Prints out a list of available commands*/
     private static void commandHelp(){
         printSeparator();
-        System.out.println("Here are the range of commands:\n 1.todo\n 2.deadline\n 3.event\n 4.list\n 5.done\n 6.bye\n ");
+        System.out.println("Here are the range of commands:\n 1.todo\n 2.deadline\n 3.event\n 4.list\n 5.done\n 6.bye");
         printSeparator();
     }
 
@@ -119,6 +219,8 @@ public class Duke {
      *           COMMAND LOGIC
      * ===========================================
      */
+
+    /*Prints welcome message*/
     private static void showWelcomeMessage() {
         printSeparator();
         String logo = "           ____          ____       \n"
@@ -130,49 +232,65 @@ public class Duke {
         System.out.println("Sup! I'm Dude!\nWhat can a brother do for you?");
         printSeparator();
     }
-    //Function to encapsulate description with square brackets
+    /**
+     * Surround text with brackets
+     *
+     * @param description  String to be placed in between brackets
+     * @return enclosedString String in between brackets
+     */
     private static String addBrackets(String description){
-        String s;
-        s = "[" + description +"]";
-        return s;
+        String enclosedString;
+        enclosedString = "[" + description +"]";
+        return enclosedString;
     }
-    //Function overload of addBrackets
+    /**
+     * Surround character with brackets
+     *
+     * @param description  Character to be placed in between brackets
+     * @return enclosedString String in between brackets
+     */
     private static String addBrackets(char description){
-        String s;
-        s = "[" + description +"]";
-        return s;
+        String enclosedString;
+        enclosedString = "[" + description +"]";
+        return enclosedString;
     }
 
-    //Command "bye" is given; Exit program
-    private static void exitProgram(){
+    /*Command "bye" is given; Exit program*/
+    private static void printExitMessage(){
         printSeparator();
         System.out.println("Bye. Hope to see you again soon!");
         printSeparator();
     }
-    //Error message when command is not recognised
-    private static void invalidCommand(){
+    /*Print message for invalid command*/
+    private static void printInvalidCommand(){
         printSeparator();
         System.out.println("Sorry! You have entered an invalid command!\n For more help, type help");
         printSeparator();
     }
-    //Function to get user input
+    /*Trim user input*/
     private static String getUserInput(){
         String inputLine = input.nextLine();
         return inputLine.trim();
     }
 
     private static void printSeparator(){
-        String line = "___________________________________________________";
+        String line = "________________________________________________________________________";
         System.out.println(line);
     }
-    //Function to get command from sentence and split sentence into 2
+    /*Function to get command from sentence and split sentence into 2*/
     private static String[] splitCommands(String userInput) {
         final String[] split = userInput.trim().split("\\s+", 2);
         return split.length == 2 ? split : new String[]{split[0], " "};
     }
-    //Function to mark as done
-    private static void markAsDone(Task[] currentTasks, int numOfTasks, String s) {
-        int taskNum = Integer.parseInt(s);
+    /**
+     * Mark task as done
+     *
+     * @param currentTasks
+     * @param numOfTasks
+     * @param number
+     */
+    private static void markAsDone(Task[] currentTasks, int numOfTasks, String number) {
+        int taskNum = Integer.parseInt(number);
 
         if(taskNum <= 0 || taskNum > numOfTasks){
             printSeparator();
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,30 @@
+public class DukeException extends Exception {
+    public static final String MISSING_DESCRIPTION = "Hey! The description cannot be empty";
+    public static final String UNINTELLIGIBLE_COMMAND = "Sorry! You have entered an invalid command!\n For more help, type \"help\"";
+    public static final String MISSING_QUALIFIER = "Hey!You are missing /by or /on in your input";
+    public static final String MISSING_INFO = "You forgot the event date/deadline";
+
+    public String errorMessage;
+
+    public DukeException(String error) {
+        switch(error) {
+        case "MISSING_DESCRIPTION":
+            this.errorMessage = MISSING_DESCRIPTION;
+            break;
+        case "UNINTELLIGIBLE_COMMAND":
+            this.errorMessage = UNINTELLIGIBLE_COMMAND;
+            break;
+        case "MISSING_QUALIFIER":
+            this.errorMessage = MISSING_QUALIFIER;
+            break;
+        case "MISSING_INFO":
+            this.errorMessage = MISSING_INFO;
+            break;
+        }
+
+    }
+
+    public String getMessage(){
+        return errorMessage;
+    }
+}
